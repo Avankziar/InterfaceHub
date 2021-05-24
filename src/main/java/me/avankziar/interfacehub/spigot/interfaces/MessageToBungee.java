a5a5c6fcd3128e5c66fdd0abe1b798268ecb013a
Avankziar <avankziar@gmail.com>
2021-05-24 06:00:18 +0200

    Build 1

--- src/main/java/me/avankziar/interfacehub/spigot/interfaces/MessageToBungee.java
+++ src/main/java/me/avankziar/interfacehub/spigot/interfaces/MessageToBungee.java
@@ -13,16 +13,34 @@ public interface MessageToBungee
 	void sendMessage(UUID uuid, String... message);
 	
 	/**
-	 * Send messages to BungeeCord proxy to multiple player with this uuid.
+	 * Send messages to BungeeCord proxy to multiple player with the uuids.
 	 * @param uuid
 	 * @param message
 	 */
 	void sendMessage(ArrayList<UUID> uuid, String... message);
-
 	
+	/**
+	 * Send messages to BungeeCord proxy to multiple player with the uuids, if the player has this permission.
+	 * If hasPermission is false, is check if the player hasnt the Permission. True for normal check.
+	 * @param uuid
+	 * @param permission
+	 * @param hasPermission
+	 * @param message
+	 */
+	void sendMessage(ArrayList<UUID> uuid, String permission, boolean hasPermission, String... message);
+
 	/**
 	 * Send messages to all Player in the BungeeCord proxy.
 	 * @param message
 	 */
 	void sendMessage(String... message);
+	
+	/**
+	 * Send messages to all Player in the BungeeCord proxy, if the players has this permission.
+	 * If hasPermission is false, is check if the player hasnt the Permission. True for normal check.
+	 * @param permission
+	 * @param hasPermission
+	 * @param message
+	 */
+	void sendMessage(String permission, boolean hasPermission, String... message);
 }
