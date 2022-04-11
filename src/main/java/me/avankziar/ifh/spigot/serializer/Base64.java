e6b5432bf3fdaaed7e9c3e46bffbfc63cfdcc515
Avankziar <avankziar@gmail.com>
2022-04-11 20:35:14 +0200

    Build 1-1-0

---
+++ src/main/java/me/avankziar/ifh/spigot/serializer/Base64.java
@@ -0,0 +1,12 @@
+package main.java.me.avankziar.ifh.spigot.serializer;
+
+public interface Base64
+{
+	public String toBase64(Object object);
+    
+    public Object fromBase64(String data);
+    
+    public String toBase64Array(Object[] objects);
+    
+    public Object[] fromBase64Array(String data);
+}
