bc2c91df6245f7b1589c6ce5a37bb12c10911e58
Avankziar <avankziar@gmail.com>
2022-07-24 10:46:10 +0200

    Build 1-3-1

--- src/main/java/me/avankziar/ifh/general/chat/Channel.java
+++ src/main/java/me/avankziar/ifh/general/chat/Channel.java
@@ -1,6 +1,6 @@
 package main.java.me.avankziar.ifh.general.chat;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * The channels are digital seperations in the chat. They are reachable with entry symbols.
@@ -11,91 +11,218 @@ import java.util.List;
 public interface Channel
 {
 	/**
-	 * Return the pluginname, which provided this.<br>
-	 * For example: SimpleChatChannels... etc.<br>
-	 * @return String
+	 * Return a list of the uniquename of the channels.
+	 * @return
 	 */
-	public String getProvider();
+	public ArrayList<String> getChannels();
+	
+	/**
+	 * Registered a channel.
+	 * <br>Return true if the channel was registered. False if not.
+	 * <br>Beware to register the channel on the system, where you want it to use. So if it should be used over the proxy
+	 * so register it there!
+	 * @param uniqueChannelName
+	 * @param symbol
+	 * @param inChatName
+	 * @param inChatColorMessage
+	 * @param Permission
+	 * @param joinPart
+	 * @param ChatFormat
+	 * @param useSpecificServer
+	 * @param useSpecificWorld
+	 * @param useBlockRadius
+	 * @param minimumTimeBetweenMessages
+	 * @param minimumTimeBetweenSameMessage
+	 * @param percentOfSimiliarityOrLess
+	 * @param timeColor
+	 * @param playernameCustomColor
+	 * @param mentionSound
+	 * @param useColor
+	 * @param useItemReplacer
+	 * @param useBookReplacer
+	 * @param useRunCommandReplacer
+	 * @param useSuggestCommandReplacer
+	 * @param useWebsiteReplacer
+	 * @param useEmojiReplacer
+	 * @param usePositionReplacer
+	 * @return
+	 */
+	public boolean registerChannel(
+			String uniqueChannelName, String symbol, String inChatName, String inChatColorMessage,
+			String Permission, String joinPart, String ChatFormat,
+			boolean useSpecificServer, boolean useSpecificWorld, int useBlockRadius, 
+			long minimumTimeBetweenMessages, long minimumTimeBetweenSameMessage, double percentOfSimiliarityOrLess,
+			String timeColor, String playernameCustomColor,
+			String mentionSound,
+			boolean useColor, boolean useItemReplacer, boolean useBookReplacer,
+			boolean useRunCommandReplacer, boolean useSuggestCommandReplacer, boolean useWebsiteReplacer,
+			boolean useEmojiReplacer, boolean usePositionReplacer);
+	
+	/**
+	 * Registered a channel. The smaller version.
+	 * <br>Return true if the channel was registered. False if not.
+	 * <br>Beware to register the channel on the system, where you want it to use. So if it should be used over the proxy
+	 * so register it there!
+	 * @param uniqueChannelName
+	 * @param symbol
+	 * @param inChatName
+	 * @param inChatColorMessage
+	 * @param Permission
+	 * @param joinPart
+	 * @param ChatFormat
+	 * @param timeColor
+	 * @param playernameCustomColor
+	 * @param mentionSound
+	 * @param useColor
+	 * @param useItemReplacer
+	 * @param useBookReplacer
+	 * @param useRunCommandReplacer
+	 * @param useSuggestCommandReplacer
+	 * @param useWebsiteReplacer
+	 * @param useEmojiReplacer
+	 * @param usePositionReplacer
+	 * @return
+	 */
+	public boolean registerChannel(
+			String uniqueChannelName, String symbol, String inChatName, String inChatColorMessage,
+			String Permission, String joinPart, String ChatFormat,
+			String timeColor, String playernameCustomColor,
+			String mentionSound,
+			boolean useColor, boolean useItemReplacer, boolean useBookReplacer,
+			boolean useRunCommandReplacer, boolean useSuggestCommandReplacer, boolean useWebsiteReplacer,
+			boolean useEmojiReplacer, boolean usePositionReplacer);
 	
 	/**
 	 * Return the entry symbol of the channel. The parameter 'NULL' as String means, that this channel has no entry symbol.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return String<br>null
+	 * @return String<br>null if channel not exist
 	 */
-	public String getSymbol(String channel);
+	public String getSymbol(String uniqueChannelName);
 	
 	/**
 	 * Return the ingame inchat format name.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return String<br>null
+	 * @return String<br>null if channel not exist
 	 */
-	public String getInChatName(String channel);
+	public String getInChatName(String uniqueChannelName);
 	
 	/**
 	 * Return the ingame inchat color code for the general message.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return String<br>null
+	 * @return String<br>null if channel not exist || InChatColor not set
 	 */
-	public String getInChatColor(String channel);
+	public String getInChatColor(String uniqueChannelName);
 	
 	/**
 	 * Return the permission of the channel.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return String<br>null
+	 * @return String<br>null if channel not exist
 	 */
-	public String getPermission(String channel);
+	public String getPermission(String uniqueChannelName);
 	
 	/**
 	 * Return the chatformat with all there replacers.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return String<br>null
+	 * @return String<br>null if channel not exist
 	 */
-	public String getChatFormat(String channel);
-	
-	/**
-	 * Return all knowable chatformat replacers.<br>
-	 * Return null if the channel dont exist.
-	 * @return List of Strings<br>null
-	 */
-	public List<String> getChatFormatReplacer();
+	public String getChatFormat(String uniqueChannelName);
 	
 	/**
 	 * Return true if this channel is set on serverlevel. Means only the message are recieved by player on the same server.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
 	 * @return
 	 */
-	public Boolean isSpecificServer(String channel);
+	public Boolean isSpecificServer(String uniqueChannelName);
 	
 	/**
 	 * Return true if this channel is set on worldlevel. Means only the message are recieved by player on the same world.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return Boolean<br>null
+	 * @return Boolean<br>null if channel not exist
 	 */
-	public Boolean isSpecificWorld(String channel);
+	public Boolean isSpecificWorld(String uniqueChannelName);
 	
 	/**
 	 * Return true if this channel is set on coordinatelevel. 
 	 * Means only the message are recieved by player on the same server and world and are maximal xx blocks away.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return Boolean<br>null
+	 * @return Boolean<br>null if channel not exist
 	 */
-	public Boolean hasBlockRadius(String channel);
+	public Boolean hasBlockRadius(String uniqueChannelName);
 	
 	/**
 	 * Return the blockradius. 0 means is isnt active.<br>
-	 * Return null if the channel dont exist.
 	 * @param channel
-	 * @return Integer<br>null
+	 * @return Integer<br>null if channel not exist
 	 */
-	public Integer getBlockRadius(String channel);
+	public Integer getBlockRadius(String uniqueChannelName);
 	
-	//INFO:More from scc channels?
+	/**
+	 * Return the used sound for the mentioning.
+	 * @param sound
+	 * @return
+	 */
+	public String getMentionSound(String uniqueChannelName);
+	
+	/**
+	 * Return true, if the channel supported colored writing.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedColor(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel itemreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedItemReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel bookreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedBookReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel runcommandreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedRunCommandReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel suggestcommandreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedSuggestCommandReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel websitereplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedWebsiteReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel emojireplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedEmojiReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel mentionreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedMentionReplacer(String uniqueChannelName);
+	
+	/**
+	 * Return true, if in the channel positionreplacer can be used.
+	 * @param uniqueChannelName
+	 * @return
+	 */
+	public boolean isUsedPositionReplacer(String uniqueChannelName);
 }
