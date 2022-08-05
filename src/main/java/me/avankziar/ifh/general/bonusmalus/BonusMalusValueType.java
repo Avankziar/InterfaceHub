5cadcb5a3444d67b8d57981999d3c7e25ab638ba
Avankziar <avankziar@gmail.com>
2022-08-05 11:53:53 +0200

    Build 1-4-0

---
+++ src/main/java/me/avankziar/ifh/general/bonusmalus/BonusMalusValueType.java
@@ -0,0 +1,11 @@
+package main.java.me.avankziar.ifh.general.bonusmalus;
+
+/**
+ * Defines, to which type of value the value set.
+ * @author Avankziar
+ *
+ */
+public enum BonusMalusValueType
+{
+	ADDITION, MULTIPLICATION;
+}
