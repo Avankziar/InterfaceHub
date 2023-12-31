17773e85285b2a05b0bbd3120093ca4a2e4b8c0d
Avankziar <avankziar@gmail.com>
2023-12-31 14:23:02 +0100

    Build 1-8-0

--- src/main/java/me/avankziar/ifh/spigot/comparison/ItemStackComparison.java
+++ src/main/java/me/avankziar/ifh/spigot/comparison/ItemStackComparison.java
@@ -12,4 +12,44 @@ public interface ItemStackComparison
 			boolean checkPotionMeta, boolean checkRepairable, boolean checkSkullMeta, boolean checkSpawnEggMeta, boolean checkTropicalFishBucketMeta);
 	
 	boolean isSimilar(ItemStack item, ItemStack filter);
+	
+	boolean matchType(ItemStack i, ItemStack f);
+	
+	boolean matchAmount(ItemStack i, ItemStack f);
+	
+	boolean matchCustomModelData(ItemStack i, ItemStack f);
+	
+	boolean matchDisplayname(ItemStack i, ItemStack f);
+	
+	boolean matchItemFlags(ItemStack i, ItemStack f);
+	
+	boolean matchLore(ItemStack i, ItemStack f);
+	
+	boolean matchEnchantments(ItemStack i, ItemStack f);
+	
+	boolean matchArmorMeta(ItemStack i, ItemStack f);
+	
+	boolean matchAxolotlBucketMeta(ItemStack i, ItemStack f);
+	
+	boolean matchBannerMeta(ItemStack i, ItemStack f);
+	
+	boolean matchBookMeta(ItemStack i, ItemStack f);
+	
+	boolean matchBundleMeta(ItemStack i, ItemStack f);
+	
+	boolean matchDamageable(ItemStack i, ItemStack f);
+	
+	boolean matchFireworkMeta(ItemStack i, ItemStack f);
+	
+	boolean matchLeatherArmorMeta(ItemStack i, ItemStack f);
+	
+	boolean matchPotionMeta(ItemStack i, ItemStack f);
+	
+	boolean matchRepairable(ItemStack i, ItemStack f);
+	
+	boolean matchSkullMeta(ItemStack i, ItemStack f);
+	
+	boolean matchSpawnEggMeta(ItemStack i, ItemStack f);
+	
+	boolean matchTropicalFishBucketMeta(ItemStack i, ItemStack f);
 }
