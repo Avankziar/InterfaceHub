6b2320cc9290798b703787d543276a49a20ba943
Avankziar <avankziar@gmail.com>
2025-03-09 07:10:37 +0100

    2-6-0

--- src/me/avankziar/ifh/general/statistic/Statistic.java
+++ src/me/avankziar/ifh/general/statistic/Statistic.java
@@ -2,9 +2,35 @@ package me.avankziar.ifh.general.statistic;
 
 import java.util.UUID;
 
+import javax.annotation.Nullable;
+
 public interface Statistic
 {
-	public Long getStatistic(UUID uuid, StatisticType statisticType);
+	/**
+	 * Return the value of the statistictype.<br>
+	 * null if the player has no entry in this statistictype.
+	 * @param uuid
+	 * @param statisticType
+	 * @return
+	 */
+	public Double getStatistic(UUID uuid, StatisticType statisticType);
 	
-	public boolean addStatistic(UUID uuid, StatisticType statisticType, long value);
+	/**
+	 * Return the value of the statistictype and the material or entitytype as string.<br>
+	 * null if the player has no entry in this statistictype.
+	 * @param uuid
+	 * @param statisticType
+	 * @param materialOrEntityType
+	 * @return
+	 */
+	public Double getStatistic(UUID uuid, StatisticType statisticType, String materialOrEntityType);
+	
+	/**
+	 * Add the value to the statistictype and/or materialOrEntityType.
+	 * @param uuid
+	 * @param statisticType
+	 * @param materialOrEntityType
+	 * @param value
+	 */
+	public void addStatisticValue(UUID uuid, StatisticType statisticType, @Nullable String materialOrEntityType, double value);
 }
